6e470da2ddd5992098a2b47f897b40200ba2bf40
Najwa Ramadhan <najwaramadhanpintar@gmail.com>
2026-09-19 22:40:53 +0700

    update ptmn2

--- Pemrograman Mobile/Pertemuan-2/ptmn2/App.js
+++ Pemrograman Mobile/Pertemuan-2/ptmn2/App.js
@@ -4,17 +4,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>Curriculum Vitae</Text>
+        <View style={styles.line} />
 
-      <Text style={styles.text}>
-        Nama Lengkap : Najwa Ramadhan{"\n"}
-        NIM : 2488010001{"\n"}
-        Asal Sekolah : MAN 3 Cirebon{"\n"}
-        Cita-cita : Illustrator{"\n"}
-        Rencana mencapai cita-cita : Melatih keterampilan dasar menggambar secara konsisten
-      </Text>
+        <Text style={styles.text}>
+          <Text style={styles.label}>Nama Lengkap : </Text>
+          Najwa Ramadhan{"\n"}
+
+          <Text style={styles.label}>NIM : </Text>
+          2488010001{"\n"}
+
+          <Text style={styles.label}>Asal Sekolah : </Text>
+          MAN 3 Cirebon{"\n"}
+
+          <Text style={styles.label}>Cita-cita : </Text>
+          Illustrator & Penulis{"\n"}
+
+          <Text style={styles.label}>Rencana Mencapai Cita-Cita : </Text>
+          Terus mengembangkan kemampuan menulis dan ilustrasi dengan rutin membuat karya, membangun portofolio, serta membagikannya di media sosial. Saat ini saya sudah mulai menulis cerita di sebuah platform sosial media dan mendapatkan respons serta pembaca yang cukup baik.
+        </Text>
+      </View>
 
       <StatusBar style="auto" />
-
     </View>
   );
 }
@@ -22,14 +34,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF0F6',
     justifyContent: 'center',
     padding: 20,
   },
 
-  text: {
+  card: {
+    backgroundColor: '#FFFFFF',
+    padding: 25,
+    borderRadius: 20,
+    elevation: 5,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
     color: '#FF69B4',
-    fontSize: 18,
-    textAlign: 'left',
+    textAlign: 'center',
+  },
+
+  line: {
+    height: 2,
+    backgroundColor: '#FFB6D9',
+    marginVertical: 15,
+  },
+
+  text: {
+    fontSize: 16,
+    lineHeight: 28,
+    color: '#444',
+  },
+
+  label: {
+    fontWeight: 'bold',
+    color: '#FF69B4',
   },
 });
